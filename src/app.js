eadaeae3137ab3870abb1bb9ac69becf6d520b06
Pranav Saluja <pranavsaluja12345@gmail.com>
2024-10-18 20:53:17 +0530

    exploring get,post

--- src/app.js
+++ src/app.js
@@ -2,12 +2,25 @@ const express = require('express')
 
 const app = express()
 
-app.use("/Pranav",(req,res)=>{
-    res.send("Hello from the server")
+//This will only handle the GET call to /user
+app.get("/user" , (req,res)=>{
+    res.send({firstName: "Akshay" , lastName:"Saini"})
 });
-app.use("/",(req,res)=>{
-    res.send("Hello from the ho")
+
+app.post("/user",(req,res)=>{
+    console.log("Save data to the database ");
+    res.send("Data successfully saved to the database");
 });
+app.delete("/user",(req,res)=>{
+  
+    res.send("Deleted Successfully");
+});
+
+//this will match all the HTTP method API Calls to /test
+app.use("/test",(req,res)=>{
+    res.send("Hello Hello Hello !")
+});
+
 
 app.listen(3000,() =>{
     console.log("Server is successfully listening on Port 3000")
